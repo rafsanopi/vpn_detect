a08ac776e8e5249b4ef80a1a09f96ffe0b4623fe
rafsanopi <rafsan3@dti.ac>
2023-12-13 00:13:55 -0800

    VPN DETECT

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,68 @@
-import 'package:check_vpn_connection/check_vpn_connection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:vpn_connection_detector/vpn_connection_detector.dart';
 
-void main() => runApp(const MainApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextButton(
-              onPressed: () async {
-                if (await CheckVpnConnection.isVpnActive()) {
-                  Fluttertoast.showToast(
-                    msg: "===========VPN Active",
-                  );
-                } else {
-                  Fluttertoast.showToast(
-                    msg: "===========VPN NOT Active",
-                  );
-                }
-              },
-              child: const Text("check_vpn_connection 0.0.2")),
-          const SizedBox(
-            height: 30,
-          ),
-          TextButton(
-              onPressed: () async {
-                final vpnDetector = VpnConnectionDetector();
-                vpnDetector.vpnConnectionStream.listen((state) {
-                  if (state == VpnConnectionState.connected) {
-                    Fluttertoast.showToast(
-                      msg: "===========VPN Active",
-                    );
-                  } else {
-                    Fluttertoast.showToast(
-                      msg: "===========VPN NOT Active",
-                    );
-                  }
-                });
-              },
-              child: const Text("vpn_connection_detector 1.0.5")),
-        ],
-      )),
-    ));
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+
+  bool vpn = true;
+
+
+  static const methodChannel = MethodChannel("method.com/vpn");
+
+
+  Future getVPNconnection() async {
+    final bool checkVPN =
+    await methodChannel.invokeMethod("isVpnConnected");
+
+    setState(() {
+      vpn = checkVPN;
+      Fluttertoast.showToast(
+          msg: "VPN is connected==$vpn",
+
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('VPN Detection'),
+        ),
+        body: Center(
+            child: TextButton(onPressed: () {
+             getVPNconnection();
+
+
+            }, child: const Text("Click"))));
   }
 }
